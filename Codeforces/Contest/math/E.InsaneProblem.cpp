7f9c1ab6283452735c234f99ebb32751741ea5d4
Saurav Kumar <saurav.k@tallysolutions.com>
2024-12-23 20:42:15 +0530

    added cf insane problem

--- Codeforces/Contest/math/E.InsaneProblem.cpp
+++ Codeforces/Contest/math/E.InsaneProblem.cpp
@@ -1,5 +1,5 @@
 /*l2 <= y = xk * *n <= r2
-l2/k**n <= x <= r2/k**n   -> n =1 till n<=1e9 multiply k erverytime so for each y range find the correct x range numbers and summation*/ 
+l2/k**n <= x <= r2/k**n   -> n =1 till n<=1e9 multiply k erverytime so for each y range find the correct x range numbers and summation*/
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -12,8 +12,8 @@ int main() {
         ll l1, r1, l2, r2, k;
         cin >> k >> l1 >> r1 >> l2 >> r2;
         ll i = 1, ans = 0;
-        for (i <= 1e9) {
-            ans += max(0LL, (min(r2 / i, r1) - max((r2 + i - 1) / i, l1)));
+        while (i <= 1e9) {
+            ans += max(0LL, 1 + (min(r2 / i, r1) - max(((l2 + i - 1) / i), l1)));
             i *= k;
         }
         cout << ans << "\n";
